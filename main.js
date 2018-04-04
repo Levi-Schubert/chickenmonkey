3e48211cfa6dee6d877c662a4133cf601c4740a6
Levi Schubert <me@levischubert.com>
2018-04-04 11:25:40 -0500

    Added comments in js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 for(let i = 1; i <= 100; i += 1){
-    if( i % 5 == 0 && i % 7 == 0){
+    if( i % 5 == 0 && i % 7 == 0){ //check if divisble by both
         console.log("ChickenMonkey");
     }
-    else if(i % 5 == 0){
+    else if(i % 5 == 0){ //check divisible by 5
         console.log("Chicken");
-    }else if(i % 7 == 0){
+    }else if(i % 7 == 0){ //check divisible by 7
         console.log("Monkey");
-    }else{
-        console.log(i);
+    }else{ 
+        console.log(i); // otherwise print number
     }
 }
