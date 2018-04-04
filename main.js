bc2c361571ce0892e08387d6296c98acd7d2a0d3
Levi Schubert <me@levischubert.com>
2018-04-04 11:11:26 -0500

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+for(let i = 1; i <= 100; i += 1){
+    if( i % 5 == 0 && i % 7 == 0){
+        console.log("ChickenMonkey");
+    }
+    else if(i % 5 == 0){
+        console.log("Chicken");
+    }else if(i % 7 == 0){
+        console.log("Monkey");
+    }else{
+        console.log(i);
+    }
+}
